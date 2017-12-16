e2f5c49ad1ad33ceafada43b5d93bda3fb3435df
huipingli <huipingli@aca80165.ipt.aol.com>
2017-12-16 14:54:52 +0800

    1111

--- junitTest/junitTest/src/main/java/com/pd/junitTest/Template.java
+++ junitTest/junitTest/src/main/java/com/pd/junitTest/Template.java
@@ -1,12 +1,14 @@
 package com.pd.junitTest;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.HashMap;
 
 import static java.util.Map.Entry;
 
 public class Template {
-	private Map<String, String> variables; // 用 散 列表 存储 变量 
+	private Map<String, String> variables; // 用 散 列表 存储 变量
 	private String templateText;
 
 	public Template(String templateText) {
@@ -20,12 +22,27 @@ public class Template {
 
 	public String evaluate() {
 		String result = templateText;
+		result = replaceVariables(result);
+		checkMissValue(result);
+		return result;
+	}
+
+	private String replaceVariables(String result) {
+		System.err.println(variables);
 		for (Entry<String, String> entry : variables.entrySet()) { // 遍历 变量
 
 			String regex = "\\$\\{" + entry.getKey()
 					+ "\\}"; /* （ 以下 2 行） 用 变量 值 替换 变量 */
 			result = result.replaceAll(regex, entry.getValue());
+
 		}
 		return result;
+	}
+
+	private void checkMissValue(String result) throws MissValueException {
+		Matcher matcher = Pattern.compile(".*\\$\\{.+\\}.*").matcher(result);
+		if (matcher.find()) {
+			throw new MissValueException("no value for " + matcher.group());
+		}
 	}
 }
--- junitTest/junitTest/src/test/java/com/pd/junitTest/TestTemplate.java
+++ junitTest/junitTest/src/test/java/com/pd/junitTest/TestTemplate.java
@@ -2,37 +2,57 @@ package com.pd.junitTest;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-public class TestTemplate {
-	@Test
-	public void oneVariable() throws Exception {
-		Template template = new Template(" Hello, ${ name}");
-		template.set(" name", "Reader");
-		assertEquals(" Hello, Reader", template.evaluate());
-	}
 
-	@Test
-	public void differentTemplate() throws Exception {
-		Template template = new Template(" Hi, ${ name}");
-		template.set(" name", "someone else");
-		assertEquals(" Hi, someone else", template.evaluate());
+import org.junit.Before;
+public class TestTemplate {
+	private Template template ;
+	
+	@Before
+	public void setUp()throws Exception{
+		template = new Template("${one},${two},${three}");
+		template.set("one", "1");
+		template.set("two", "2");
+		template.set("three", "3");
 	}
+	
 
 	@Test
 	public void multipleVariables() throws Exception {
 
-		Template template = new Template("${ one}, ${ two}, ${ three}");
-		template.set(" one", "1");
-		template.set(" two", "2");
-		template.set(" three", "3");
 
-		assertEquals(" 1, 2, 3", template.evaluate());
+		assertTemplateEvaluatesTo("1,2,3");
+	}
+
+
+	private void assertTemplateEvaluatesTo(String expected) {
+		String templateEnd = template.evaluate();
+//		System.out.println(templateEnd);
+		assertEquals(expected, templateEnd);
 	}
 
 	@Test
 	public void unknownVariablesAreIgnored() throws Exception {
-		Template template = new Template(" Hello, ${ name}");
-		template.set(" name", "Reader");
-		template.set(" doesnotexist", "Hi");
-		assertEquals(" Hello, Reader", template.evaluate());
+		template.set("doesnotexist", "whatever");//不在 模版里面，不能替换
+		assertTemplateEvaluatesTo("1,2,3");
+	}
+	
+	@Test
+	public void missValueRaisesException(){
+		try {
+			new Template("${foo}").evaluate();
+			fail("evaluate shoud throw a exception ,when a variable is left without a value");
+		} catch (MissValueException e) {
+			assertEquals("no value for ${foo}", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void variablesGetProcessedJustOnce()throws Exception{
+		template.set("one", "${one}");
+		template.set("two", "${three}");
+		template.set("three", "${two}");
+		
+		assertTemplateEvaluatesTo("${one},${three},${two}");
+		
 	}
 }
