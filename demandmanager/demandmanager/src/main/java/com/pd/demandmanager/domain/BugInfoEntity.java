6ec0308cec5f950ff6f0cff0c7b29b940e8ba97d
panda0120 <m18280054202@163.com>
2018-03-26 20:27:18 +0800

    create program

---
+++ demandmanager/demandmanager/src/main/java/com/pd/demandmanager/domain/BugInfoEntity.java
@@ -0,0 +1,99 @@
+package com.pd.demandmanager.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_bug_infos")
+public class BugInfoEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	private String userId;
+	private String username;
+	private Long createTime;
+	private Long modifyTime;
+	private Long endTime;
+	private String content;
+	private String charger;
+	private Integer status;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Long getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Long createTime) {
+		this.createTime = createTime;
+	}
+
+	public Long getModifyTime() {
+		return modifyTime;
+	}
+
+	public void setModifyTime(Long modifyTime) {
+		this.modifyTime = modifyTime;
+	}
+
+	public Long getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Long endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCharger() {
+		return charger;
+	}
+
+	public void setCharger(String charger) {
+		this.charger = charger;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+	
+}
